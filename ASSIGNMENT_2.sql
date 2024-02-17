73430d891e3adc5793f71fe38fc177c5ff8a29c8
HemankBajaj <hemankbajaj05@gmail.com>
2024-02-17 22:53:06 +0530

    bug fixes

--- ASSIGNMENT_2.sql
+++ ASSIGNMENT_2.sql
@@ -1,3 +1,9 @@
+CREATE TABLE department (
+    dept_id CHAR(3) PRIMARY KEY NOT NULL,
+    dept_name VARCHAR(40) NOT NULL UNIQUE
+);
+
+
 CREATE TABLE student (
     student_id CHAR(11) PRIMARY KEY NOT NULL,
     first_name VARCHAR(40) NOT NULL,
@@ -14,20 +20,38 @@ CREATE TABLE courses (
     course_name VARCHAR(20) NOT NULL UNIQUE,
     course_desc TEXT,
     credits NUMERIC NOT NULL CHECK (credits > 0),
-    dept_id CHAR(3) REFERENCES department(dept_id)
+    dept_id CHAR(3) REFERENCES department(dept_id),
+    CONSTRAINT check_course_id_format 
+        CHECK (
+            SUBSTRING(course_id FROM 1 FOR 3) = dept_id 
+            AND SUBSTRING(course_id FROM 4 FOR 3) ~ '^\d{3}$'
+        )
+
 );
 
-CREATE TABLE student_courses (
+CREATE  TABLE student_courses (
     student_id CHAR(11)  REFERENCES student(student_id),
     course_id CHAR(6)  REFERENCES courses(course_id),
     session VARCHAR(9) NOT NULL,
     semester INTEGER NOT NULL CHECK (semester IN (1, 2)),
     grade NUMERIC NOT NULL CHECK (grade >= 0 AND grade <= 10),
     PRIMARY KEY (student_id, course_id)
-    FOREIGN KEY (course_id, session, semester) REFERENCES course_offers(course_id, session, semester)
+    -- FOREIGN KEY (course_id, session, semester) REFERENCES course_offers(course_id, session, semester)
 );
 
-CREATE TABLE course_offers (
+CREATE TABLE professor (
+    professor_id VARCHAR(10) PRIMARY KEY NOT NULL,
+    professor_first_name VARCHAR(40) NOT NULL,
+    professor_last_name VARCHAR(40) NOT NULL,
+    office_number VARCHAR(20),
+    contact_number CHAR(10) NOT NULL,
+    start_year INTEGER NOT NULL,
+    resign_year INTEGER NOT NULL CHECK (resign_year >= start_year),
+    dept_id CHAR(3) NOT NULL REFERENCES department(dept_id)
+);
+
+
+CREATE  TABLE course_offers (
     course_id CHAR(6) NOT NULL REFERENCES courses(course_id),
     session VARCHAR(9) NOT NULL,
     semester INTEGER NOT NULL CHECK (semester IN (1, 2)),
@@ -37,16 +61,6 @@ CREATE TABLE course_offers (
     PRIMARY KEY (course_id, session, semester)
 );
 
-CREATE TABLE professor (
-    professor_id VARCHAR(10) PRIMARY KEY NOT NULL,
-    first_name VARCHAR(40) NOT NULL,
-    last_name VARCHAR(40) NOT NULL,
-    office_number VARCHAR(20),
-    contact_number CHAR(10) NOT NULL,
-    start_year INTEGER NOT NULL,
-    resign_year INTEGER NOT NULL CHECK (resign_year >= start_year),
-    dept_id CHAR(3) NOT NULL REFERENCES department(dept_id)
-);
 
 CREATE TABLE valid_entry (
     dept_id CHAR(3) NOT NULL REFERENCES department(dept_id),
@@ -55,10 +69,6 @@ CREATE TABLE valid_entry (
     PRIMARY KEY (dept_id, entry_year)
 );
 
-CREATE TABLE department (
-    dept_id CHAR(3) PRIMARY KEY NOT NULL,
-    dept_name VARCHAR(40) NOT NULL UNIQUE
-);
 
 -- 2.1.1, 2.1.3
 -- validate student id, validate email_id
@@ -69,7 +79,7 @@ DECLARE
     new_entry_year INTEGER; 
     new_dept_id CHAR(3); 
     seq_number INTEGER; 
-    new_email_id VARCHAR(50)
+    new_email_id VARCHAR(50); 
     new_student_id VARCHAR(11); 
     email_domain VARCHAR(50);
 BEGIN
@@ -80,15 +90,15 @@ BEGIN
     -- Check if entry year and dept id are valid in valid_entry table
     IF NOT EXISTS (
         SELECT * FROM valid_entry 
-        WHERE dept_id = NEW.dept_id  AND entry_year = NEW.entry_year
+        WHERE valid_entry.dept_id = NEW.dept_id  AND valid_entry.entry_year = new_entry_year
     ) THEN
         RAISE EXCEPTION 'invalid';
     END IF;
 
-    SELECT seq_number INTO dept_seq_number 
+    SELECT valid_entry.seq_number INTO dept_seq_number 
     FROM valid_entry 
-    WHERE dept_id = new_dept_id 
-    AND entry_year = new_entry_year;
+    WHERE valid_entry.dept_id = new_dept_id 
+    AND valid_entry.entry_year = new_entry_year;
 
     -- Validate Sequence Number 
     -- if not validated then raise 'invalid' exception
@@ -110,7 +120,7 @@ BEGIN
     -- 2) new_student_id == NEW.student_id 
 
     -- Check if email domain matches dept_id + 'iitd.ac.in' and student_id components match
-    IF email_domain <> student_id_dept_id || '.iitd.ac.in' OR new_student_id <> NEW.student_id
+    IF email_domain <> new_dept_id || '.iitd.ac.in' OR new_student_id <> NEW.student_id THEN
         RAISE EXCEPTION 'invalid';
     END IF;
 
@@ -124,17 +134,20 @@ FOR EACH ROW
 EXECUTE FUNCTION validate_student_id_function();
 
 
--- 2.1.2
-CREATE OR REPLACE FUNCTION update_seq_number_function() RETURNS TRIGGER AS $$
+-- -- 2.1.2
+CREATE OR REPLACE FUNCTION update_seq_number_function() 
+RETURNS TRIGGER AS $$
+DECLARE
+    new_entry_year INTEGER; 
 BEGIN
 
     -- fetch the new entry details 
-    entry_year := LEFT(NEW.student_id, 4)::INTEGER;
+    new_entry_year := LEFT(NEW.student_id, 4)::INTEGER;
 
     
     UPDATE valid_entry
     SET seq_number = seq_number + 1
-    WHERE dept_id = NEW.dept_id AND entry_year = NEW.entry_year;
+    WHERE valid_entry.dept_id = NEW.dept_id AND valid_entry.entry_year = new_entry_year;
 
     RETURN NEW;
 END;
@@ -146,7 +159,7 @@ FOR EACH ROW
 EXECUTE FUNCTION update_seq_number_function();
 
 
--- 2.1.4
+-- -- 2.1.4
 
 CREATE TABLE student_dept_change (
     old_student_id CHAR(11) NOT NULL,
@@ -183,7 +196,7 @@ BEGIN
     END IF;
 
     -- if department is not changed then simply update whatever is done
-    IF OLD.dept_id = NEW.dept_id
+    IF OLD.dept_id = NEW.dept_id THEN 
         RETURN NEW; 
     END IF; 
 
@@ -238,7 +251,7 @@ SELECT
     sc.course_id,
     sc.session,
     sc.semester,
-    COUNT(unique sc.student_id) AS number_of_students,
+    COUNT(distinct sc.student_id) AS number_of_students,
     AVG(sc.grade) AS average_grade,
     MAX(sc.grade) AS max_grade,
     MIN(sc.grade) AS min_grade
@@ -253,18 +266,18 @@ GROUP BY
 CREATE OR REPLACE FUNCTION update_course_eval()
 RETURNS TRIGGER AS $$
 BEGIN
-    REFRESH MATERIALIZED VIEW course_eval;
+    -- REFRESH MATERIALIZED VIEW course_eval;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER course_eval_update_trigger
-INSTEAD OF INSERT OR UPDATE ON student_courses
+AFTER INSERT OR UPDATE ON student_courses
 FOR EACH STATEMENT
 EXECUTE FUNCTION update_course_eval();
 
 
--- 2.2.2
+-- -- 2.2.2
 
 CREATE OR REPLACE FUNCTION update_student_tot_credits()
 RETURNS TRIGGER AS $$
@@ -294,7 +307,7 @@ AFTER INSERT ON student_courses
 FOR EACH ROW
 EXECUTE FUNCTION update_student_tot_credits();
 
--- 2.2.3
+-- -- 2.2.3
 CREATE OR REPLACE FUNCTION check_course_enrollment_limit()
 RETURNS TRIGGER AS $$
 DECLARE
@@ -334,18 +347,23 @@ BEFORE INSERT ON student_courses
 FOR EACH ROW
 EXECUTE FUNCTION check_course_enrollment_limit();
 
--- 2.2.4
+-- -- 2.2.4
 CREATE OR REPLACE FUNCTION check_course_first_year()
 RETURNS TRIGGER AS $$
 DECLARE
     student_first_year INTEGER;
     session_year INTEGER; 
+    course_credits INTEGER; 
 BEGIN
     -- Get the first year of the student from their student ID
     student_first_year := CAST(SUBSTRING(NEW.student_id FROM 1 FOR 4) AS INTEGER);
     session_year := CAST(SUBSTRING(NEW.session FROM 1 FOR 4) AS INTEGER); 
 
-    IF NEW.credits = 5 AND student_first_year <> session_year THEN
+    SELECT credits INTO course_credits
+    FROM courses 
+    WHERE courses.course_id = NEW.course_id; 
+
+    IF course_credits = 5 AND student_first_year <> session_year THEN
         RAISE EXCEPTION 'invalid';
     END IF;
 
@@ -359,7 +377,7 @@ FOR EACH ROW
 EXECUTE FUNCTION check_course_first_year();
 
 
--- 2.2.5
+-- -- 2.2.5
 
 CREATE OR REPLACE VIEW student_semester_summary AS
 SELECT
@@ -381,7 +399,18 @@ CREATE OR REPLACE FUNCTION update_student_semester_summary()
 RETURNS TRIGGER AS $$
 DECLARE
     semester_credits INTEGER;
+    course_credits INTEGER; 
+    old_course_credits INTEGER; 
 BEGIN
+
+    SELECT credits INTO course_credits
+    FROM courses 
+    WHERE courses.course_id = NEW.course_id; 
+
+    SELECT credits INTO old_course_credits
+    FROM courses 
+    WHERE courses.course_id = OLD.course_id; 
+
     IF TG_OP = 'INSERT' THEN
         -- credits in the current semester 
         SELECT SUM(c.credits)
@@ -392,28 +421,28 @@ BEGIN
         AND sc.session = NEW.session
         AND sc.semester = NEW.semester;
 
-        IF semester_credits + NEW.credits > 26 THEN
+        IF semester_credits + course_credits > 26 THEN
             RAISE EXCEPTION 'invalid';
         END IF;
 
         UPDATE student
-        SET tot_credits = tot_credits + NEW.credits
+        SET tot_credits = tot_credits + course_credits
         WHERE student_id = NEW.student_id;
 
     ELSIF TG_OP = 'UPDATE' THEN
         UPDATE student
-        SET tot_credits = tot_credits - OLD.credits + NEW.credits
+        SET tot_credits = tot_credits - old_course_credits + course_credits
         WHERE student_id = NEW.student_id;
 
     ELSIF TG_OP = 'DELETE' THEN
         UPDATE student
-        SET tot_credits = tot_credits - OLD.credits
+        SET tot_credits = tot_credits - old_course_credits
         WHERE student_id = OLD.student_id;
 
     END IF;
 
     -- Update student semester summary view
-    REFRESH MATERIALIZED VIEW student_semester_summary;
+    -- REFRESH MATERIALIZED VIEW student_semester_summary;
 
     RETURN NEW;
 END;
@@ -421,24 +450,24 @@ $$ LANGUAGE plpgsql;
 
 -- INSERT TRIGGER
 CREATE TRIGGER update_student_semester_summary_insert_trigger
-INSTEAD OF INSERT ON student_courses
+AFTER INSERT ON student_courses
 FOR EACH ROW
 EXECUTE FUNCTION update_student_semester_summary();
 
 -- UPDATE TRIGGER
 CREATE TRIGGER update_student_semester_summary_update_trigger
-INSTEAD OF UPDATE ON student_courses
+AFTER UPDATE ON student_courses
 FOR EACH ROW
 EXECUTE FUNCTION update_student_semester_summary();
 
 -- DELETE TRIGGER 
 CREATE TRIGGER update_student_semester_summary_delete_trigger
-INSTEAD OF DELETE ON student_courses
+AFTER DELETE ON student_courses
 FOR EACH ROW
 EXECUTE FUNCTION update_student_semester_summary();
 
 
--- 2.2.6
+-- -- 2.2.6
 CREATE OR REPLACE FUNCTION check_course_capacity()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -446,7 +475,6 @@ BEGIN
     IF (SELECT enrollments >= capacity FROM course_offers WHERE course_id = NEW.course_id AND session = NEW.session AND semester = NEW.semester) THEN
         RAISE EXCEPTION 'course is full';
     ELSE
-        INSERT INTO student_courses VALUES (NEW.*);
         -- Update the number of enrollments in course_offers
         UPDATE course_offers 
         SET enrollments = enrollments + 1 
@@ -463,10 +491,18 @@ FOR EACH ROW
 EXECUTE FUNCTION check_course_capacity();
 
 
--- 2.3.1
+-- -- 2.3.1
 CREATE OR REPLACE FUNCTION remove_course_entries()
 RETURNS TRIGGER AS $$
+DECLARE 
+    course_credits INTEGER; 
 BEGIN
+
+
+    SELECT credits into course_credits
+    FROM courses 
+    WHERE course_id = OLD.course_id; 
+
     -- clear from student_courses
     DELETE FROM student_courses
     WHERE course_id = OLD.course_id
@@ -561,76 +597,84 @@ EXECUTE FUNCTION check_course_offers_entry();
 
 
 
--- 2.4.1
-CREATE OR REPLACE FUNCTION update_department_courses()
+-- -- 2.4.1
+CREATE OR REPLACE FUNCTION update_department_trigger()
 RETURNS TRIGGER AS $$
 BEGIN
-    IF OLD.dept_id <> NEW.dept_id THEN
-        -- Update course ids in course_offers table
-        UPDATE course_offers
-        SET course_id = NEW.dept_id || substring(course_id from 4)
-        WHERE course_id LIKE OLD.dept_id || '___';
-
-        -- Update course ids in courses table
-        UPDATE courses
-        SET course_id = NEW.dept_id || substring(course_id from 4)
-        WHERE course_id LIKE OLD.dept_id || '___';
-
-        -- Update course ids in student_courses table
-        UPDATE student_courses
-        SET course_id = NEW.dept_id || substring(course_id from 4)
-        WHERE course_id LIKE OLD.dept_id || '___';
-
-        -- Update course ids in professor table
+    IF NEW.dept_id <> OLD.dept_id THEN
+        INSERT INTO department (dept_id, dept_name)
+        VALUES (NEW.dept_id, 'DUMMY');
+        
         UPDATE professor
         SET dept_id = NEW.dept_id
         WHERE dept_id = OLD.dept_id;
 
-        -- Update course ids in student table
+        ALTER TABLE student DISABLE TRIGGER ALL;
+        ALTER TABLE course_offers DISABLE TRIGGER ALL;
+        ALTER TABLE student_courses DISABLE TRIGGER ALL;
+        ALTER TABLE department DISABLE TRIGGER ALL;
+
         UPDATE student
         SET dept_id = NEW.dept_id
         WHERE dept_id = OLD.dept_id;
+        
+
+
+        UPDATE courses
+        SET course_id = NEW.dept_id || SUBSTRING(course_id FROM 4)
+        WHERE course_id LIKE OLD.dept_id || '%';
+
+
+        
+        UPDATE course_offers
+        SET course_id = NEW.dept_id || SUBSTRING(course_id FROM 4)
+        WHERE course_id LIKE OLD.dept_id || '%';
+        
+
+        
+        UPDATE student_courses
+        SET course_id = NEW.dept_id || SUBSTRING(course_id FROM 4)
+        WHERE course_id LIKE OLD.dept_id || '%';
+        
+
+
+        DELETE FROM department 
+        WHERE dept_id = NEW.dept_id; 
+
+        ALTER TABLE student ENABLE TRIGGER ALL;
+        ALTER TABLE department ENABLE TRIGGER ALL;
+        ALTER TABLE course_offers ENABLE TRIGGER ALL;
+        ALTER TABLE student_courses ENABLE TRIGGER ALL;
+
+
     END IF;
 
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-
-
-CREATE OR REPLACE FUNCTION delete_department()
+CREATE OR REPLACE FUNCTION delete_department_trigger()
 RETURNS TRIGGER AS $$
 BEGIN
-    -- Check if there are any students in the department
     IF EXISTS (SELECT 1 FROM student WHERE dept_id = OLD.dept_id) THEN
         RAISE EXCEPTION 'Department has students';
     ELSE
-        -- Delete all courses in the department from course_offers table
-        DELETE FROM course_offers WHERE course_id LIKE OLD.dept_id || '___';
-
-        -- Delete all courses in the department from courses table
-        DELETE FROM courses WHERE course_id LIKE OLD.dept_id || '___';
-
-        -- Delete all professors in the department from professor table
+        -- DELETE PROFS
         DELETE FROM professor WHERE dept_id = OLD.dept_id;
-
-        -- Delete the department record
-        DELETE FROM department WHERE dept_id = OLD.dept_id;
+        -- DELETE COURSES
+        DELETE FROM courses WHERE course_id LIKE OLD.dept_id || '%';
+        DELETE FROM course_offers WHERE course_id LIKE OLD.dept_id || '%';
     END IF;
-
     RETURN OLD;
 END;
 $$ LANGUAGE plpgsql;
 
--- Delete Trigger
+CREATE TRIGGER update_department_trigger
+AFTER UPDATE ON department
+FOR EACH ROW
+EXECUTE FUNCTION update_department_trigger();
+
 CREATE TRIGGER delete_department_trigger
 BEFORE DELETE ON department
 FOR EACH ROW
-EXECUTE FUNCTION delete_department();
-
--- update trigger
-CREATE TRIGGER update_department_courses_trigger
-BEFORE UPDATE ON department
-FOR EACH ROW
-EXECUTE FUNCTION update_department_courses();
-
+EXECUTE FUNCTION delete_department_trigger();
